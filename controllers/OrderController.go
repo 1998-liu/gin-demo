50ab3373eaf41387477984f506950532568f6eca
liuhuan <liuhuan@liuhuandeMacBook-Pro.local>
2023-09-26 23:12:43 +0800

    [feat]学习控制器中接受参数的各种方法

--- controllers/OrderController.go
+++ controllers/OrderController.go
@@ -1,9 +1,32 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 type OrderController struct{}
 
+type Search struct {
+	Id      int    `json:"id"`
+	OrderNo string `json:"order_no"`
+}
+
 func (o OrderController) GetList(c *gin.Context) {
-	ReturnSuccess(c, 200, "success", "order_list", 2)
+	//接收 form 表单参数
+	// id := c.PostForm("id")
+	// orderNo := c.DefaultPostForm("order_no", "order123")
+	// ReturnSuccess(c, 200, "success", "id:"+id+","+"orderNo:"+orderNo, 2)
+
+	//使用 map 接收json参数
+	// param := make(map[string]interface{}, 5)
+	// err := c.BindJSON(&param)
+
+	//使用结构体接收 json 参数
+	param := &Search{}
+	err := c.BindJSON(&param)
+	if err == nil {
+		ReturnSuccess(c, 200, "success", param, 1)
+		return
+	}
+	ReturnError(c, 4001, gin.H{"err": err})
 }
--- controllers/UserController.go
+++ controllers/UserController.go
@@ -1,13 +1,30 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 type UserController struct{}
 
 func (u UserController) GetUserInfo(c *gin.Context) {
-	ReturnSuccess(c, 0, "success", "user_info", 1)
+	id := c.Query("id")
+	name := c.DefaultQuery("name", "Gopher")
+	userInfo := map[string]string{
+		"id":   id,
+		"name": name,
+	}
+	ReturnSuccess(c, 0, "success", userInfo, 1)
 }
 
 func (u UserController) GetUserList(c *gin.Context) {
-	ReturnError(c, 4004, "获取列表错误")
+	id, _ := strconv.Atoi(c.PostForm("id"))
+	name := c.PostForm("name")
+	userList := map[string]interface{}{
+		"id":   id,
+		"name": name,
+	}
+	// ReturnError(c, 4004, "获取列表错误")
+	ReturnSuccess(c, 200, "success", userList, 1)
 }
